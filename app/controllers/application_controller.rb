14be687b2d3ad48b6668087772601dafb7f9d675
Austin Longo <austinlongocontracting@gmail.com>
2023-01-15 02:49:49 -0500

    removed unnessecary functions in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
     before_action :authorized
-
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: {count: session[:count]}
-    end
-    
+   
     def authorized
         return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
     end
